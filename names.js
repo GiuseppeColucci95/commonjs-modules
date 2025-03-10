a6d797d51e5fbae54dad2913e3ffc63dafd9a0bc
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-10 14:51:14 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,16 @@
+/**
+ * Return an object constructed with the given parameters
+ * @param {string} firstName first name to append to the return object
+ * @param {string} lastName last name to append to the return object
+ * @returns {object}
+ */
+function givesFullNameObj(firstName, lastName) {
+
+  return {
+    firstName,
+    lastName
+  };
+}
+
+//export of the function
+module.exports = givesFullNameObj;
