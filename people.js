109509dd7f70b321b3444b08114ed80bbc50146d
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-10 15:02:44 +0100

    add third js file

---
+++ people.js
@@ -0,0 +1,23 @@
+//import functions from names.js and hobbies.js files
+const fullNameFunction = require('./names.js');
+const hobbiesFunction = require('./hobbies.js');
+
+/**
+ * Return an object containing person fullName and hobbies
+ * @returns {object} the person object
+ */
+function describePerson() {
+
+  const fullName = fullNameFunction('Giuseppe', 'Colucci');
+  const hobbies = hobbiesFunction('To develop', 'To read', 'To train');
+
+  const person = {
+    fullName,
+    hobbies
+  };
+
+  return person;
+}
+
+//print the result
+console.log(describePerson());
